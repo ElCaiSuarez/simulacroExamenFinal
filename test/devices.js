bcc3640814ebcba5ffcfb7ed5408ddbea34fa9d8
Juan Carlos Suarez <caisuarez@gmail.com>
2022-11-24 23:33:12 -0300

    inicializo test device

--- test/devices.js
+++ test/devices.js
@@ -1,1 +1,29 @@
-//todo
+const { assert } = require('chai')
+const request = require('supertest')
+const app = require('../app')
+const { Device } = require('../db/models');
+
+const identifierTest = "NEW123456789";
+
+describe('Devices', function () {
+    let deviceId
+    let crearDatosTest = async function () {
+        var deviceCreado = await Device.create({
+            identifier: identifierTest
+        })
+        deviceId = deviceCreado.id;
+    }
+    before(crearDatosTest);
+    after(async function () {
+        await Device.destroy({
+            where: { id: deviceId },
+        })
+    });
+    it('GET/devices', async function () {
+        return request(app)
+            .get('/devices')
+            .then(res => {
+                assert.equal(res.status, 200);
+            })
+    })
+})
